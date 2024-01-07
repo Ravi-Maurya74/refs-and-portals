a95cb1e71db0576d8d1db906ebf6109a9ee7013b
Ravi <lcs2020043@iiitl.ac.in>
2024-01-07 11:32:18 +0530

    TimerChallenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChanllenege from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChanllenege title="Easy" targetTime={1} />
+        <TimerChanllenege title="Not easy" targetTime={5} />
+        <TimerChanllenege title="Getting tough" targetTime={10} />
+        <TimerChanllenege title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
